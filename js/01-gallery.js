93c4f000eb5b7105758c94da3381e2ad88cada40
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-01-27 16:38:19 +0200

    hw 7 task 1  - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onClickOpenModal);
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+function onClickOpenModal(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", onClickCloseModal);
+
+  function onClickCloseModal(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
